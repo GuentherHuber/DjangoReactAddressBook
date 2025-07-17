18524372513b5bd9a5c7ffa078e830ee04418957
Guenther Huber <guenther.huber@sokratel.com>
2025-07-17 16:15:09 +0200

    Worked on issue #10

--- backend/addressbook/serializers.py
+++ backend/addressbook/serializers.py
@@ -1,48 +1,56 @@
 from rest_framework import serializers
 from .models import Address
 import os
+from django.db import IntegrityError
 
 class AddressSerializer(serializers.ModelSerializer):
-    class Meta:
+      class Meta:
             model=Address
             fields=['id','first_name','last_name','city','street_name','house_number','postcode','profile_picture']
 
-    def validate_first_name(self,value):
-          if not value.strip():
-                raise serializers.ValidationError("Vorname darf nicht leer sein!")
-          return value
+      #Verhindere dopplete Einträge
+      def validate(self, data):
+            if Address.objects.filter(first_name=data.get('first_name'),last_name=data.get('last_name'),city=data.get('city'),
+                                      street_name=data.get('street_name'),house_number=data.get('house_number'),postcode=data.get('postcode')).exists():
+                  raise serializers.ValidationError("Eintrag im Adressbuch bereits vorhanden!")
+            return data
 
-    def validate_last_name(self,value):
-          if not value.strip():
-                raise serializers.ValidationError("Nachname darf nicht leer sein!")
-          return value
-    
-    def validate_city(self,value):
-          if not value.strip():
-                raise serializers.ValidationError("Stadt darf nicht leer sein!")
-          return value
-    
-    def validate_street_name(self,value):
-          if not value.strip():
-                raise serializers.ValidationError("Strasse darf nicht leer sein!")
-          return value
-    
-    def validate_house_number(self,value):
-          if not value.strip():
-                raise serializers.ValidationError("Hausnummer darf nicht leer sein!")
-          return value
-    
-    def validate_postcode(self,value):
-          if len(str(value))!=5:
-                raise serializers.ValidationError("PLZ muss aus genau 5 Ziffern bestehen!")
-          return value
-    
-    def validate_profile_picture(self,value):
-        valid_extensions = ['.jpg','.jpeg','.png','.gif']
-        extension=os.path.splitext(value.name)[1].lower()
-        if extension not in valid_extensions:
-            raise serializers.ValidationError("Ungültiges Dateiformat!")
-        return value
+      def validate_first_name(self,value):
+            if not value.strip():
+                  raise serializers.ValidationError("Vorname darf nicht leer sein!")
+            return value
+
+      def validate_last_name(self,value):
+            if not value.strip():
+                  raise serializers.ValidationError("Nachname darf nicht leer sein!")
+            return value
+      
+      def validate_city(self,value):
+            if not value.strip():
+                  raise serializers.ValidationError("Stadt darf nicht leer sein!")
+            return value
+      
+      def validate_street_name(self,value):
+            if not value.strip():
+                  raise serializers.ValidationError("Strasse darf nicht leer sein!")
+            return value
+      
+      def validate_house_number(self,value):
+            if not value.strip():
+                  raise serializers.ValidationError("Hausnummer darf nicht leer sein!")
+            return value
+      
+      def validate_postcode(self,value):
+            if len(str(value))!=5:
+                  raise serializers.ValidationError("PLZ muss aus genau 5 Ziffern bestehen!")
+            return value
+      
+      def validate_profile_picture(self,value):
+            valid_extensions = ['.jpg','.jpeg','.png','.gif']
+            extension=os.path.splitext(value.name)[1].lower()
+            if extension not in valid_extensions:
+                  raise serializers.ValidationError("Ungültiges Dateiformat!")
+            return value    
     
 
             
--- backend/addressbook/test/test_Api.py
+++ backend/addressbook/test/test_Api.py
@@ -4,12 +4,13 @@ from ..models import Address
 import os
 
 class AddressApiTest(APITestCase):
-    
-
-    def test_create_valid_address(self):
+    picturePath=[]
+    initialData=None
+    #Wird zu beginn einmal ausgeführt. Angelegte DB-Daten sind automatisch persistent
+    def setUp(self):
         base_dir=(os.path.dirname(os.path.abspath(__file__)))
         with open (os.path.join(base_dir,'artifacts','profilePicture.jpg'),'rb') as imageFile:
-            data={
+            self.initialData={
                 'first_name':'Max',
                 'last_name':'Mustermann',
                 'city':'Teststadt',
@@ -18,10 +19,63 @@ class AddressApiTest(APITestCase):
                 'postcode':12345,
                 'profile_picture':imageFile
             }
+            
+            response=self.client.post('/addressbook/api/',self.initialData,format='multipart')
+            
 
+            splittedPictureUrl=response.data['profile_picture'].split('http://testserver/')[1]
+            self.picturePath.append(os.path.abspath(os.path.join(base_dir,'..','..',splittedPictureUrl)))
+
+    #Wird am Ende einmal ausgeführt. Hochgeladene Files werden gelöscht
+    def tearDown(self):
+        for file in self.picturePath:
+            if os.path.exists(file):
+                os.remove(file)
+    
+    #Teste ob GET-Methode für initialData funktioniert
+    def test_get_initial_address(self):
+        response=self.client.get('/addressbook/api/')
+        self.assertEqual(len(response.data),1)
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+        self.assertEqual(Address.objects.filter(id=response.data[0]['id']).exists(),True) #Prüfe ob DB-Eintrag überhaupt existiert; Müsste eigentlich im Test für Model/DB stattfiden
+        self.assertEqual(response.data[0]['first_name'],self.initialData['first_name'])
+        self.assertEqual(response.data[0]['last_name'],self.initialData['last_name'])
+        self.assertEqual(response.data[0]['city'],self.initialData['city'])
+        self.assertEqual(response.data[0]['street_name'],self.initialData['street_name'])
+        self.assertEqual(response.data[0]['house_number'],self.initialData['house_number'])
+        self.assertEqual(response.data[0]['postcode'],self.initialData['postcode'])
+        self.assertTrue('/media/images/profilePicture' in response.data[0]['profile_picture'])
+        self.assertTrue(response.data[0]['profile_picture'].endswith('.jpg'))
+
+    def test_similar_address(self):
+        base_dir=(os.path.dirname(os.path.abspath(__file__)))
+        with open (os.path.join(base_dir,'artifacts','profilePicture.jpg'),'rb') as imageFile:
+            self.initialData['profile_picture']=imageFile
+            response=self.client.post('/addressbook/api/',self.initialData,format='multipart')
+        self.assertEqual(response.status_code,status.HTTP_400_BAD_REQUEST)
+        self.assertIn('Eintrag im Adressbuch bereits vorhanden!',str(response.data))
+
+    
+    def test_new_address(self):
+        base_dir=(os.path.dirname(os.path.abspath(__file__)))
+        with open (os.path.join(base_dir,'artifacts','profilePicture.jpg'),'rb') as imageFile:
+            data={
+                'first_name':'Lise',
+                'last_name':'Huber',
+                'city':'Hauptstadt',
+                'street_name':'Hauptstraße',
+                'house_number':'123',
+                'postcode':54321,
+                'profile_picture':imageFile
+            }
+            
             response=self.client.post('/addressbook/api/',data,format='multipart')
+        
+            splittedPictureUrl=response.data['profile_picture'].split('http://testserver/')[1]
+            self.picturePath.append(os.path.abspath(os.path.join(base_dir,'..','..',splittedPictureUrl)))
+
             self.assertEqual(response.status_code,status.HTTP_201_CREATED)
-            self.assertEqual(Address.objects.filter(id=response.data['id']).exists(),True) #Prüfe ob DB-Eintrag überhaupt existiert; Müsste eigentlich im Test für Model/DB stattfiden
+            self.assertTrue(Address.objects.filter(id=response.data['id']).exists())
             self.assertEqual(response.data['first_name'],data['first_name'])
             self.assertEqual(response.data['last_name'],data['last_name'])
             self.assertEqual(response.data['city'],data['city'])
@@ -30,9 +84,4 @@ class AddressApiTest(APITestCase):
             self.assertEqual(response.data['postcode'],data['postcode'])
             self.assertTrue('/media/images/profilePicture' in response.data['profile_picture'])
             self.assertTrue(response.data['profile_picture'].endswith('.jpg'))
-            #Hier ist der Test zu Ende. Das für den Test hochgeladene ProfilePicture muss aber wieder gelöscht werden
-            #Erst mal Pfad zur Datei ermitteln
-            splittedPictureUrl=response.data['profile_picture'].split('http://testserver/')[1]
-            picturePath=os.path.abspath(os.path.join(base_dir,'..','..',splittedPictureUrl))
-            if os.path.exists(picturePath):
-                os.remove(picturePath)
+
